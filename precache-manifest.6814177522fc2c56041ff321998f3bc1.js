56531074e6c11f8f69436d0257f82d9dfab0a0ec
Song Cho <iamsongcho@gmail.com>
2021-04-26 22:22:30 +0900

    Updates

---
+++ precache-manifest.6814177522fc2c56041ff321998f3bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467b1d87e80a1489f53985c381eff437",
+    "url": "/tag.it.easy.client-lite/index.html"
+  },
+  {
+    "revision": "0136e2de6a5dde6b5ec5",
+    "url": "/tag.it.easy.client-lite/static/css/main.8d64b04c.chunk.css"
+  },
+  {
+    "revision": "34bd647bcc88116349d9",
+    "url": "/tag.it.easy.client-lite/static/js/2.2a5b5b4b.chunk.js"
+  },
+  {
+    "revision": "a6ce63b9a32c708cd9a5e8317a710c27",
+    "url": "/tag.it.easy.client-lite/static/js/2.2a5b5b4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0136e2de6a5dde6b5ec5",
+    "url": "/tag.it.easy.client-lite/static/js/main.1c43cbcb.chunk.js"
+  },
+  {
+    "revision": "ca89e8130d43ae6106a3",
+    "url": "/tag.it.easy.client-lite/static/js/runtime-main.4dcebd25.js"
+  },
+  {
+    "revision": "f6a534b1951cd7c4c17fe041371ca0dc",
+    "url": "/tag.it.easy.client-lite/static/media/logo.f6a534b1.png"
+  }
+]);
